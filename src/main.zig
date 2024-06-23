2df100077f9e14406b617b2b9abf8e20de4ba227
Kyle Loveless <kjloveless321@gmail.com>
2024-06-23 09:06:22 -0500

    remove boilerplate init code. read user input from stdin.

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,27 @@
 const std = @import("std");
+const io = std.io;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdin = io.getStdIn().reader();
+    var buffer: [1024]u8 = undefined;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    while (try nextLine(stdin, &buffer) != null) {
+        //
+    }
+}
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
+    const line = (try reader.readUntilDelimiterOrEof(
+            buffer,
+            '\n',
+    )) orelse return null;
 
-    try bw.flush(); // don't forget to flush!
+    // trim windows-only carriage return char
+    if (@import("builtin").os.tag == .windows) {
+        return std.mem.trimRight(u8, line, "\r");
+    } else {
+        return line;
+    }
 }
 
 test "simple test" {
