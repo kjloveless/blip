dfa162d6c74912f38e5cd2cbee21c25b36a4752d
Kyle Loveless <kjloveless321@gmail.com>
2024-07-03 13:30:04 -0500

    add 'Home' and 'End' support

--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,8 @@ const editorKey = enum(u8) {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN,
 };
@@ -170,10 +172,20 @@ fn editorReadKey(reader: std.fs.File.Reader) !u8 {
                 if (try reader.read(seq[2..3]) != 1) return '\x1b';
                 if (seq[2] == '~') {
                     switch (seq[1]) {
+                        '1' => return @intFromEnum(editorKey.HOME_KEY),
+                        '4' => return @intFromEnum(editorKey.END_KEY),
                         '5' => return @intFromEnum(editorKey.PAGE_UP),
                         '6' => return @intFromEnum(editorKey.PAGE_DOWN),
+                        '7' => return @intFromEnum(editorKey.HOME_KEY),
+                        '8' => return @intFromEnum(editorKey.END_KEY),
                         else => {},
                     }
+                }
+            } else if (seq[0] == 'O') {
+                switch (seq[1]) {
+                    'H' => return @intFromEnum(editorKey.HOME_KEY),
+                    'F' => return @intFromEnum(editorKey.END_KEY),
+                    else => {},
                 }
             } else {
                 switch (seq[1]) {
@@ -181,6 +193,8 @@ fn editorReadKey(reader: std.fs.File.Reader) !u8 {
                     'B' => return @intFromEnum(editorKey.ARROW_DOWN),
                     'C' => return @intFromEnum(editorKey.ARROW_RIGHT),
                     'D' => return @intFromEnum(editorKey.ARROW_LEFT),
+                    'H' => return @intFromEnum(editorKey.HOME_KEY),
+                    'F' => return @intFromEnum(editorKey.END_KEY),
                     else => {},
                 }
             }
@@ -346,6 +360,8 @@ fn editorProcessKeypress(reader: std.fs.File.Reader) !void {
             _ = try posix.write(posix.STDOUT_FILENO, "\x1b[H");
             posix.exit(0);
         },
+        @intFromEnum(editorKey.HOME_KEY) => E.cx = 0,
+        @intFromEnum(editorKey.END_KEY) => E.cx = E.screencols - 1,
         @intFromEnum(editorKey.PAGE_UP), @intFromEnum(editorKey.PAGE_DOWN) => {
             var times: u16 = E.screenrows;
             while (times > 0) : (times -= 1) {
